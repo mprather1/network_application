4b2f12c499f7ae07794d7832b8c9ddeacf72d601
Michael Prather <mprather1@gmail.com>
2016-09-18 14:10:41 -0500

    all tests passing

--- lib/networkApplication.rb
+++ lib/networkApplication.rb
@@ -44,5 +44,24 @@ class NetworkApplication
     @snippet = Snippet.new
     @snippet.load_snippet(name)
   end
+  
+  def run(name)
+    find_snippet(name)
+    @snippet.hostnames.each do |host|
+      Net::SSH.start(host, @snippet.user) do |ssh|
+        hostname = ssh.exec! 'hostname'
+        puts "Executing #{@snippet.function} on #{hostname}..."
+        ssh.open_channel do |channel|
+          channel.exec @snippet.function do |ch, success|
+            abort "Error" unless success
+            channel.on_data do |ch, data|
+              puts "#{data}"
+            end
+          end
+        end
+        ssh.loop
+      end
+    end
+  end
 
 end
--- spec/networkApplication_spec.rb
+++ spec/networkApplication_spec.rb
@@ -1,15 +1,49 @@
 require 'networkApplication'
+require 'snippet'
+
+name = "undtgradt"
+username = 'mike'
+function = "sudo apt-get update"
+hostnames = "192.168.0.25", "192.168.0.7"
+
+describe 'Snippet' do
+  
+  describe 'attributes' do
+    it 'allows for reading and writing for :name' do
+      snippet = Snippet.new
+      snippet.name = "snippetName"
+      expect(snippet.name).to eq('snippetName')
+    end
+    
+    it 'allows for reading and writing for :hostnames' do
+      snippet = Snippet.new
+      snippet.hostnames = 'snippetHostname1', 'snippetHostname2'
+      expect(snippet.hostnames).to eq(['snippetHostname1', 'snippetHostname2'])
+    end
+    
+  end
+  
+  describe 'functions' do
+    
+    it "allows for saving and loading of snippets" do
+      snippet = Snippet.new
+      snippet.save_snippet(name, username, function, hostnames)
+      # snippet.load_snippet(name)
+      expect(snippet.name).to eq(name)
+      expect(snippet.username).to eq(username)
+      expect(snippet.function).to eq(function)
+      expect(snippet.hostnames).to eq(hostnames)
+
+    end
+  end
+end  
 
 describe "networkApplication" do
   
   describe 'find_snippet' do
     it "finds and checks for correct snippet" do
       n = NetworkApplication.new
-      name = "testSnippet"
-      username = 'snippetUsername'
-      function = "snippetFunction"
-      hostnames = "snippetHostname1", "snippetHostname2"
-      n.find_snippet('testSnippet')
+      n.find_snippet(name)
       expect(n.snippet.name).to eq(name)
       expect(n.snippet.username).to eq(username)
       expect(n.snippet.function).to eq(function)
