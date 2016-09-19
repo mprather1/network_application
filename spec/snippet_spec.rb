4370eecd29be15d11a6e2b2a9fb6e1bc4fb94d2d
Michael Prather <mprather1@gmail.com>
2016-09-18 19:15:39 -0500

    fixed tests

--- spec/snippet_spec.rb
+++ spec/snippet_spec.rb
@@ -1,31 +1,43 @@
 require 'snippet'
 
+name = "snippetTest"
+username = 'mike'
+function = "sudo apt-get update"
+hostnames = "192.168.0.25", "192.168.0.7"
+
 describe 'Snippet' do
-  
+
   describe 'attributes' do
     it 'allows for reading and writing for :name' do
       snippet = Snippet.new
       snippet.name = "snippetName"
       expect(snippet.name).to eq('snippetName')
     end
-    
+
+    it 'allows for reading and writing for :username' do
+      snippet = Snippet.new
+      snippet.username = "snippetUsername"
+      expect(snippet.username).to eq('snippetUsername')
+    end
+
+    it 'allows for reading and writing for :function' do
+      snippet = Snippet.new
+      snippet.function = "snippetFunction"
+      expect(snippet.function).to eq('snippetFunction')
+    end
+
     it 'allows for reading and writing for :hostnames' do
       snippet = Snippet.new
       snippet.hostnames = 'snippetHostname1', 'snippetHostname2'
       expect(snippet.hostnames).to eq(['snippetHostname1', 'snippetHostname2'])
     end
-    
+
   end
-  
+
   describe 'functions' do
-    
+
     it "allows for saving and loading of snippets" do
       snippet = Snippet.new
-      name = "testSnippet"
-      username = 'snippetUsername'
-      function = "snippetFunction"
-      hostnames = "snippetHostname1", "snippetHostname2"
-      
       snippet.save_snippet(name, username, function, hostnames)
       # snippet.load_snippet(name)
       expect(snippet.name).to eq(name)
@@ -35,5 +47,4 @@ describe 'Snippet' do
 
     end
   end
-  
 end
